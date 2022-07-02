38fcdc6105aeaffac6a9b4a4c65b7ce420caae6d
killerdroid099 <ajempire09@gmail.com>
2022-07-02 15:52:03 +0530

    Express server setup

--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const workouts = require("../routes/workouts");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", (req, res) => {
-	res.json({ name: "sam" });
-});
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
 
-app.listen(4000, () => {
-	console.log("listening at port 4000");
+app.use(bodyParser.json());
+
+app.use("/", workouts);
+
+app.listen(process.env.PORT, async () => {
+	try {
+		await mongoose.connect(process.env.MONGODB_URI);
+		console.log(`listening at port ${process.env.PORT} and connected to db`);
+	} catch (error) {
+		console.log({ error });
+	}
 });
